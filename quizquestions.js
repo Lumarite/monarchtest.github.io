a83d4bf579f98b380036f8530a2f066164043475
Lumarite <77590345+Lumarite@users.noreply.github.com>
2021-01-19 01:18:06 -0500

    Update quizquestions.js

--- quizquestions.js
+++ quizquestions.js
@@ -31,139 +31,13 @@ questions = [
     },
 //Imperial vs Isolationist
     {
-        "question": "The end goal of a nation is to have global control.",
+        "question": "TImperialism.",
         "i18n": {
         },
         "effect": {
             "a": 0,
             "b": 0,
             "c": -5,
-            "d": 0,
-            "e": 0,
-            "f": 0,
-            "g": 0
-        }
-    },
-    {
-        "question": "It is of my best interest to spread my rule.",
-        "i18n": {
-        },
-        "effect": {
-            "a": 0,
-            "b": 0,
-            "c": -5,
-            "d": 0,
-            "e": 0,
-            "f": 0,
-            "g": 0
-        }
-    },
-    {
-        "question": "The strong should conquer the weak.",
-        "i18n": {
-        },
-        "effect": {
-            "a": 0,
-            "b": 0,
-            "c": -5,
-            "d": 0,
-            "e": 0,
-            "f": 0,
-            "g": 0
-        }
-    },
-    {
-        "question": "War creates a weak nation.",
-        "i18n": {
-        },
-        "effect": {
-            "a": 0,
-            "b": 0,
-            "c": 5,
-            "d": 0,
-            "e": 0,
-            "f": 0,
-            "g": 0
-        }
-    },
-    {
-        "question": "There is no benefit from prolonged warfare.",
-        "i18n": {
-        },
-        "effect": {
-            "a": 0,
-            "b": 0,
-            "c": 5,
-            "d": 0,
-            "e": 0,
-            "f": 0,
-            "g": 0
-        }
-    },
-    {
-        "question": "A nation to keep to itself.",
-        "i18n": {
-        },
-        "effect": {
-            "a": 0,
-            "b": 0,
-            "c": 5,
-            "d": 0,
-            "e": 0,
-            "f": 0,
-            "g": 0
-        }
-    },
-    {
-        "question": "Social Darwinism is good",
-        "i18n": {
-        },
-        "effect": {
-            "a": 0,
-            "b": 0,
-            "c": -5,
-            "d": 0,
-            "e": 0,
-            "f": 0,
-            "g": 0
-        }
-    },
-    {
-        "question": "Complete self reliance is an ideal to work towards.",
-        "i18n": {
-        },
-        "effect": {
-            "a": 0,
-            "b": 0,
-            "c": 5,
-            "d": 0,
-            "e": 0,
-            "f": 0,
-            "g": 0
-        }
-    },
-    {
-        "question": "The Earth belongs to the King.",
-        "i18n": {
-        },
-        "effect": {
-            "a": 0,
-            "b": 0,
-            "c": -5,
-            "d": 0,
-            "e": 0,
-            "f": 0,
-            "g": 0
-        }
-    },
-     {
-        "question": "Imperialismis a threat to natuonal covreignty",
-        "i18n": {
-        },
-        "effect": {
-            "a": 0,
-            "b": 0,
-            "c": 5,
             "d": 0,
             "e": 0,
             "f": 0,
@@ -172,77 +46,7 @@ questions = [
     },
 //Absolute vs Constitutional
     {
-        "question": "Only the king knows what's good for the nation.",
-        "i18n": {
-        },
-        "effect": {
-            "a": 0,
-            "b": 0,
-            "c": 0,
-            "d": -5,
-            "e": 0,
-            "f": 0,
-            "g": 0
-        }
-    },
-    {
-        "question": "The King must keep his authority by any means necessary.",
-        "i18n": {
-        },
-        "effect": {
-            "a": 0,
-            "b": 0,
-            "c": 0,
-            "d": -5,
-            "e": 0,
-            "f": 0,
-            "g": 0
-        }
-    },
-    {
-        "question": "A failsafe is necessary for preventing tyranny.",
-        "i18n": {
-        },
-        "effect": {
-            "a": 0,
-            "b": 0,
-            "c": 0,
-            "d": 5,
-            "e": 0,
-            "f": 0,
-            "g": 0
-        }
-    },
-    {
-        "question": "A parliaent is needed in a monarchy.",
-        "i18n": {
-        },
-        "effect": {
-            "a": 0,
-            "b": 0,
-            "c": 0,
-            "d": 5,
-            "e": 0,
-            "f": 0,
-            "g": 0
-        }
-    },
-    {
-        "question": "The King must abide by a strict constitution.",
-        "i18n": {
-        },
-        "effect": {
-            "a": 0,
-            "b": 0,
-            "c": 0,
-            "d": 5,
-            "e": 0,
-            "f": 0,
-            "g": 0
-        }
-    },
-    {
-        "question": "Strong leadership is a key aspect of Monarchism.",
+        "question": "Absolute.",
         "i18n": {
         },
         "effect": {
@@ -257,7 +61,7 @@ questions = [
     },
 //Religious vs Secular
     {
-        "question": "There should be a national religion.",
+        "question": "Religion.",
         "i18n": {
         },
         "effect": {
@@ -266,83 +70,13 @@ questions = [
             "c": 0,
             "d": 0,
             "e": 5,
-            "f": 0,
-            "g": 0
-        }
-    },
-    {
-        "question": "A theocratic monarchy brings stability in a nation.",
-        "i18n": {
-        },
-        "effect": {
-            "a": 0,
-            "b": 0,
-            "c": 0,
-            "d": 0,
-            "e": 5,
-            "f": 0,
-            "g": 0
-        }
-    },
-    {
-        "question": "There should be no seperation of the church and the state.",
-        "i18n": {
-        },
-        "effect": {
-            "a": 0,
-            "b": 0,
-            "c": 0,
-            "d": 0,
-            "e": 5,
-            "f": 0,
-            "g": 0
-        }
-    },
-    {
-        "question": "Peace comes from tolerating other religions.",
-        "i18n": {
-        },
-        "effect": {
-            "a": 0,
-            "b": 0,
-            "c": 0,
-            "d": 0,
-            "e": -5,
-            "f": 0,
-            "g": 0
-        }
-    },
-    {
-        "question": "All religions must be seen as equal by the King.",
-        "i18n": {
-        },
-        "effect": {
-            "a": 0,
-            "b": 0,
-            "c": 0,
-            "d": 0,
-            "e": -5,
-            "f": 0,
-            "g": 0
-        }
-    },
-    {
-        "question": "Theocracy distorts religion.",
-        "i18n": {
-        },
-        "effect": {
-            "a": 0,
-            "b": 0,
-            "c": 0,
-            "d": 0,
-            "e": -5,
             "f": 0,
             "g": 0
         }
     },
 //Historical vs Modern
     {
-        "question": "Society was better in the past",
+        "question": "Historical.",
         "i18n": {
         },
         "effect": {
@@ -352,76 +86,6 @@ questions = [
             "d": 0,
             "e": 0,
             "f": -5,
-            "g": 0
-        }
-    },
-    {
-        "question": "The French Revolution should not have happened.",
-        "i18n": {
-        },
-        "effect": {
-            "a": 0,
-            "b": 0,
-            "c": 0,
-            "d": 0,
-            "e": 0,
-            "f": -5,
-            "g": 0
-        }
-    },
-    {
-        "question": "The Industrial Revolution and its consequences have been a disaster for the human race.",
-        "i18n": {
-        },
-        "effect": {
-            "a": 0,
-            "b": 0,
-            "c": 0,
-            "d": 0,
-            "e": 0,
-            "f": -5,
-            "g": 0
-        }
-    },
-    {
-        "question": "Technology needs to be advanced as much as possible.",
-        "i18n": {
-        },
-        "effect": {
-            "a": 0,
-            "b": 0,
-            "c": 0,
-            "d": 0,
-            "e": 0,
-            "f": 5,
-            "g": 0
-        }
-    },
-    {
-        "question": " Modern technology should be used to keep monarchism relevant.",
-        "i18n": {
-        },
-        "effect": {
-            "a": 0,
-            "b": 0,
-            "c": 0,
-            "d": 0,
-            "e": 0,
-            "f": 5,
-            "g": 0
-        }
-    },
-    {
-        "question": "Transhumanism is a good ideology.",
-        "i18n": {
-        },
-        "effect": {
-            "a": 0,
-            "b": 0,
-            "c": 0,
-            "d": 0,
-            "e": 0,
-            "f": 5,
             "g": 0
         }
     },
